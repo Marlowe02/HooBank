4de5fa121654704faa5bca0079da4507285675bc
Marlowe02 <154118075+Marlowe02@users.noreply.github.com>
2024-03-31 16:08:37 +0800

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Testimonials from "./Testimonials";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Business from "./Business";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import GetStarted from "./GetStarted";
+import FeedbackCard from "./FeedbackCard";
+import Button from "./Button";
+import NavBar from "./NavBar";
+
+export { Testimonials, Clients, CTA, Footer, Hero, Stats, Business, Billing, CardDeal, NavBar, GetStarted, FeedbackCard, Button };
